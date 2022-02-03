3e505079ada472b0f6dcb92623960f611f3d3207
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-02-02 20:23:25 -0500

    port-fix

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const {animals} = require('./data/animals.json')
+
+const PORT = process.env.PORT || 3001;
+
 const app = express()
 
 function filterByQuery(query, animalsArray) {
@@ -62,6 +65,8 @@ app.get('/api/animals/:id', (req, res) => {
     }
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+
+  
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
